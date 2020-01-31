09694d496ef2d6324e59fa3d2cd55b0734440f53
veritas <veritas9872@gmail.com>
2020-01-31 13:08:03 +0900

    Verified that grid search does not cause problems with logs. Increased batch size for better performance. Also, fixed seeds and reproducibility settings have been adopted for grid search.

--- train/grid_search.py
+++ train/grid_search.py
@@ -13,22 +13,26 @@ def grid_search():
         teacher_checkpoint='../records/Teacher/Trial 08  2020-01-30 14-22-32/checkpoints/checkpoint_040.tar',
         train_method='Search',
         num_epochs=400,
-        batch_size=256,
-        num_workers=2,
+        batch_size=512,
+        num_workers=4,
         lr=0.1,
         gpu=1
     )
-    temperatures = [1, 2, 4, 8, 16, 32]
-    distill_ratios = [0.99, 0.95, 0.9, 0.75, 0.5, 0.25, 0.1, 0.05, 0.01]
+    temperatures = [1, 2, 4, 8, 16, 32, 64]
+    distill_ratios = [1., 0.99, 0.95, 0.9, 0.75, 0.5, 0.25, 0.1, 0.05, 0.01, 0.]
 
     for temp in temperatures:
         for dist in distill_ratios:
             options['temperature'] = temp
             options['distill_ratio'] = dist
             opt = knowledge_distillation_options(**options).parse_args()
+            # Reproducibility settings. Seeding must be repeated at the start of every run.
+            torch.random.manual_seed(9872)
             main(opt)
 
 
 if __name__ == '__main__':
-    torch.backends.cudnn.benchmark = True  # Increase speed if input sizes are the same.
+    # Reproducibility settings.
+    torch.backends.cudnn.benchmark = False
+    torch.backends.cudnn.deterministic = True
     grid_search()
